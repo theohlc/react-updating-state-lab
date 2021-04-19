dbdd8b40f66702ef39ea41a998e74764f93101bc
theo <teddyhoover@gmail.com>
2021-04-19 12:18:14 -0500

    commit tree 9d5be8498758aeca61bfb7d2ad2606099ed20804

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,30 @@
+import React from "react"
+
 // Code DigitalClicker Component Here
+export default class DigitalClicker extends React.Component {
+    constructor(props){
+        super()
+
+        this.state = {
+            timesClicked: 0
+        }
+    }
+    
+    render() {
+        return(
+            <button onClick={this.handleClick}>
+                {this.state.timesClicked}
+            </button>
+        )
+    }
+
+    handleClick = () => {
+        this.setState(
+            previousState => {
+                return{
+                    timesClicked: (previousState.timesClicked + 1)
+                }   
+            }
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,48 @@
+import React from "react";
+
 // Code YouTubeDebugger Component Here
+export default class YouTubeDebugger extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <button className='bitrate' onClick={this.bitrateHandleBtn}>bitrate</button>
+                <button className='resolution' onClick={this.resolutionHandleBtn}>res</button>
+            </div>
+        )
+    }
+
+    bitrateHandleBtn = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+
+    resolutionHandleBtn = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    resolution: '720p'
+                }
+            }
+        })
+    }
+}
